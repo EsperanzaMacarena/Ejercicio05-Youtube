aca619a72e621bf1ea7f25d99fe401de18a0703e
Gonzalo Jesús Punta Pérez <gjpunta@aulas.triana>
2020-01-20 09:05:57 +0100

    layout terminado

--- Ejercicio05/app/src/main/java/com/escacena/ejercicio05/MyVideoRecyclerViewAdapter.java
+++ Ejercicio05/app/src/main/java/com/escacena/ejercicio05/MyVideoRecyclerViewAdapter.java
@@ -37,7 +37,7 @@ public class MyVideoRecyclerViewAdapter extends RecyclerView.Adapter<MyVideoRecy
     @Override
     public void onBindViewHolder(final ViewHolder holder, int position) {
         holder.mItem = mValues.get(position);
-        holder.mIdView.setText(mValues.get(position).id);
+     //   holder.mIdView.setText(mValues.get(position).id);
         holder.mContentView.setText(mValues.get(position).content);
 
         holder.mView.setOnClickListener(new View.OnClickListener() {
@@ -59,14 +59,14 @@ public class MyVideoRecyclerViewAdapter extends RecyclerView.Adapter<MyVideoRecy
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         public final View mView;
-        public final TextView mIdView;
+    //    public final TextView mIdView;
         public final TextView mContentView;
         public DummyItem mItem;
 
         public ViewHolder(View view) {
             super(view);
             mView = view;
-            mIdView = (TextView) view.findViewById(R.id.item_number);
+    //        mIdView = (TextView) view.findViewById(R.id.item_number);
             mContentView = (TextView) view.findViewById(R.id.content);
         }
 
